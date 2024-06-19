f12657bc7e04c5c95ccdc2af0216e888e0daaec1
Guilherme C <souguiga@gmail.com>
2024-06-19 08:37:16 -0300

    V 1.2.0

--- MDC.js
+++ MDC.js
@@ -9,24 +9,21 @@ process.stdin.on('data', function(data)
     if(!n1)
         {
             n1=ent;
-            maior=n1
+            maior=n1;
         }
     else if(!n2)
         {
             n2=ent;
             if(n2>n1)
                 {
-                    maior=n2
+                    maior=n2;
                 }
-        }
-    else
-        {   
             for(i=maior;i>0;i--)
                 {
                     if(n1%i==0&&n2%i==0)
                         {
                             console.log('O MDC entre '+n1+' e '+n2+' é '+i);
-                            process.exit()
+                            process.exit();
                         }
                 }
         }
